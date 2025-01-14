9facaa4dee86a04a79f52f7722a73341bd04e5a5
Hamish Mackintosh-Keiller <h.mack.keiller@gmail.com>
2025-01-14 23:49:02 +1000

    add axios for API calls and implement data fetching in App component. create custom axos get hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDebounce } from "./hooks/useDebounce";
 import { SearchFilter } from "./components/SearchFilter";
 import { ContactForm } from "./components/ContactForm";
 import { PersonsList } from "./components/PersonsList";
 import { Person } from "./types/Types";
+import axios from "axios";
 
 export default function App() {
-  const [persons, setPersons] = useState<Person[]>([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  // const [persons, setPersons] = useState<Person[]>([
+  //   { name: "Arto Hellas", number: "040-123456", id: 1 },
+  //   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  //   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  //   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  // ]);
+  const [persons, setPersons] = useState<Person[]>([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterInput, setFilterInput] = useState("");
 
-  const debouncedValue = useDebounce(filterInput, 350);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/persons");
+        setPersons(response.data);
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          console.error(err.message);
+        } else {
+          console.error(err);
+        }
+      }
+    };
+    fetchData();
+  }, []);
 
+  const debouncedValue = useDebounce(filterInput, 350);
   const debouncedValueLowerCase = debouncedValue.toLowerCase();
 
   return (
