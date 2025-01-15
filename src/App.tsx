290dd9377ab7f529c3d315ad5f495e9da1345fed
Hamish Mackintosh-Keiller <h.mack.keiller@gmail.com>
2025-01-15 11:32:41 +1000

    refactor: remove commented-out initial state for persons in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,6 @@ import { Person } from "./types/Types";
 import axios from "axios";
 
 export default function App() {
-  // const [persons, setPersons] = useState<Person[]>([
-  //   { name: "Arto Hellas", number: "040-123456", id: 1 },
-  //   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-  //   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  //   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  // ]);
   const [persons, setPersons] = useState<Person[]>([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
