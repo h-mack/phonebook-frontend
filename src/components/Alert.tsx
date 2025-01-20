dc00bf1699a257206d9193d0bcc86411dbcfcadc
Hamish Mackintosh-Keiller <h.mack.keiller@gmail.com>
2025-01-20 17:42:52 +1000

    add font awesome icon to alert component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,5 +1,12 @@
 import { ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 
 export function Alert({ children }: { children: ReactNode }) {
-  return <p className="alert">{children}</p>;
+  return (
+    <p className="alert">
+      <FontAwesomeIcon icon={faCircleCheck} style={{ paddingRight: ".5rem" }} />
+      {children}
+    </p>
+  );
 }
